346c12bfca645f2566e229b287f8525d732c8cdc
Joey Faris <joeyfaris1@yahoo.com>
2022-06-07 10:38:13 -0700

    commit tree f83609bb6da4827095c60d151442cec7bbb5cb61

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = function() {
+    return 2000 / 100
+}
+
+const square = double => {
+    return 2 * 2
+}
+
+
+const add = (parameter1, parameter2) => {
+    return parameter1 + parameter2
+}
